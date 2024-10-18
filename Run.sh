b5067879a1b2dbd9cf42dec9e5c3eaf5d878ed57
Michael Anderson <52722296+and02709@users.noreply.github.com>
2024-10-18 16:29:29 -0500

    basic maf scripts

---
+++ Run.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=2GB
+#SBATCH --time=1:00:00
+#SBATCH -p msismall
+#SBATCH --mail-type=FAIL  
+#SBATCH --mail-user=and02709@umn.edu 
+#SBATCH -o MAF.out
+#SBATCH -e MAF.err
+#SBATCH --job-name MAF
+
+
+# Check for command line arguments
+if [ $# -eq 0 ]; then
+  echo "No arguments provided."
+  show_help
+  exit 1
+fi
+
+wd=$(pwd)
+
+# *** Make sure you have a new enough getopt to handle long options (see the man page)
+getopt -T &>/dev/null
+if [[ $? -ne 4 ]]; then echo "Getopt is too old!" >&2 ; exit 1 ; fi
+
+declare {wd,input_directory,input_file_name,path_to_github_repo,user_x500,use_crossmap,use_genome_harmonizer,use_rfmix,make_report,custom_qc,help}
+OPTS=$(getopt -u -o '' -a --longoptions 'wd:,input_directory:,input_file_name:,path_to_github_repo:,user_x500:,use_crossmap:,use_genome_harmonizer:,use_rfmix:,make_report:,custom_qc:,help' -n "$0" -- "$@")
+    # *** Added -o '' ; surrounted the longoptions by ''
+if [[ $? -ne 0 ]] ; then echo "Failed parsing options." >&2 ; exit 1 ; fi
+    # *** This has to be right after the OPTS= assignment or $? will be overwritten
+
+set -- $OPTS
+    # *** As suggested by chepner
+
+while [[ $# -gt 0 ]]; do
+  key=$1
+  case $key in
+	--wd )
+		wd=$2
+		shift 2
+		;;
+	--file )
+		file=$2
+		shift 2
+		;;
+	--maf )
+		maf=$2
+		shift 2
+		;;
+    --)
+			shift
+			break
+			;;
+    	*)
+  esac
+done
+
+
+echo "working directory: $wd"
+echo "input file name: $file"
+echo "minor allele frequency: $maf"
+
+mkdir maf_files
+
+sbatch --time 1:00:00 --mem 8GB --array 1-22 --wait -N1 maf_individual.sh ${wd} ${file} ${maf}
+
+
+exit 0
---
+++ maf_individual.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=4GB
+#SBATCH --time=2:00:00
+#SBATCH -p agsmall
+#SBATCH -o rfmix_%a.out
+#SBATCH -e rfmix_%a.err
+#SBATCH --job-name rfmix
+
+WD=$1
+FILE=$2
+MAF=$3
+CHR=$SLURM_ARRAY_TASK_ID
+
+module load singularity
+
+cd $WORK/maf_files
+
+singularity exec my_image.sif plink --bfile $file --chr $CHR --maf $MAF --make-bed --out maf_${CHR}
