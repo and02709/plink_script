a2cef0d2797cfe94fbd870f2b5161e1f0c15815c
Michael Anderson <52722296+and02709@users.noreply.github.com>
2024-10-24 09:42:26 -0500

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -19,10 +19,20 @@ if [ $# -eq 0 ]; then
 fi
 
 wd=$(pwd)
+
 # *** Make sure you have a new enough getopt to handle long options (see the man page)
 getopt -T &>/dev/null
 if [[ $? -ne 4 ]]; then echo "Getopt is too old!" >&2 ; exit 1 ; fi
-@@ -26,10 +23,6 @@ set -- $OPTS
+
+declare {wd,input_directory,input_file_name,path_to_github_repo,user_x500,use_crossmap,use_genome_harmonizer,use_rfmix,make_report,custom_qc,help}
+OPTS=$(getopt -u -o '' -a --longoptions 'wd:,input_directory:,input_file_name:,path_to_github_repo:,user_x500:,use_crossmap:,use_genome_harmonizer:,use_rfmix:,make_report:,custom_qc:,help' -n "$0" -- "$@")
+    # *** Added -o '' ; surrounted the longoptions by ''
+if [[ $? -ne 0 ]] ; then echo "Failed parsing options." >&2 ; exit 1 ; fi
+    # *** This has to be right after the OPTS= assignment or $? will be overwritten
+
+set -- $OPTS
+    # *** As suggested by chepner
+
 while [[ $# -gt 0 ]]; do
   key=$1
   case $key in
@@ -33,9 +43,19 @@ while [[ $# -gt 0 ]]; do
 	--file )
 		file=$2
 		shift 2
-@@ -46,14 +39,12 @@ while [[ $# -gt 0 ]]; do
+		;;
+	--maf )
+		maf=$2
+		shift 2
+		;;
+    --)
+			shift
+			break
+			;;
+    	*)
   esac
 done
+
 
 echo "working directory: $wd"
 echo "input file name: $file"
@@ -44,4 +64,5 @@ echo "minor allele frequency: $maf"
 mkdir maf_files
 
 sbatch --time 1:00:00 --mem 8GB --array 1-22 --wait -N1 maf_individual.sh ${wd} ${file} ${maf}
+
 exit 0
