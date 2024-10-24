aa209efde1599f990a7fedafffed040bd433ac76
Michael Anderson <52722296+and02709@users.noreply.github.com>
2024-10-24 10:15:14 -0500

    Update maf_individual.sh

--- maf_individual.sh
+++ maf_individual.sh
@@ -3,16 +3,17 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=4GB
-#SBATCH --time=2:00:00
+#SBATCH --time=0:30:00
 #SBATCH -p agsmall
 #SBATCH -o rfmix_%a.out
 #SBATCH -e rfmix_%a.err
 #SBATCH --job-name rfmix
 
-FILE=$1
-MAF=$2
+WD=$1
+FILE=$2
+MAF=$3
 CHR=$SLURM_ARRAY_TASK_ID
 
 module load singularity
 
-singularity exec plink.sif plink --bfile /input/$file --chr $CHR --maf $MAF --make-bed --out /output/maf_${CHR}
+singularity exec plink.sif plink --bfile $FILE --chr $CHR --maf $MAF --make-bed --out /${WD}/maf_${CHR}
