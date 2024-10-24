44bc1ea717dab8dea4e6cc9fc1b979f6b5d8e81a
Michael Anderson <52722296+and02709@users.noreply.github.com>
2024-10-24 09:34:44 -0500

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=2GB
+#SBATCH --time=1:00:00
+#SBATCH -p msismall
+#SBATCH --mail-type=FAIL  
+#SBATCH --mail-user=and02709@umn.edu 
+#SBATCH -o MAF.out
+#SBATCH -e MAF.err
+#SBATCH --job-name MAF
 
 $file=SMILES_GDA
 $maf=0.01
