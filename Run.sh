6b9dafa52ced9a85890d64a753441948eebb4461
Michael Anderson <52722296+and02709@users.noreply.github.com>
2024-10-24 09:41:02 -0500

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -11,13 +11,37 @@
 #SBATCH -e MAF.err
 #SBATCH --job-name MAF
 
-$file=SMILES_GDA
-$maf=0.01
+# Check for command line arguments
+if [ $# -eq 0 ]; then
+  echo "No arguments provided."
+  show_help
+  exit 1
+fi
 
+wd=$(pwd)
+# *** Make sure you have a new enough getopt to handle long options (see the man page)
+getopt -T &>/dev/null
+if [[ $? -ne 4 ]]; then echo "Getopt is too old!" >&2 ; exit 1 ; fi
+@@ -26,10 +23,6 @@ set -- $OPTS
+while [[ $# -gt 0 ]]; do
+  key=$1
+  case $key in
+	--wd )
+		wd=$2
+		shift 2
+		;;
+	--file )
+		file=$2
+		shift 2
+@@ -46,14 +39,12 @@ while [[ $# -gt 0 ]]; do
+  esac
+done
+
+echo "working directory: $wd"
 echo "input file name: $file"
 echo "minor allele frequency: $maf"
 
-sbatch --time 0:10:00 --mem 2GB --array 1-22 --wait -N1 /plink_script/maf_individual.sh ${file} ${maf}
+mkdir maf_files
 
-
+sbatch --time 1:00:00 --mem 8GB --array 1-22 --wait -N1 maf_individual.sh ${wd} ${file} ${maf}
 exit 0
